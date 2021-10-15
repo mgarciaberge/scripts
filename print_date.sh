a5a6e91cdf688f9cc09a84def2393d69660c119d
Manel Garcia Berge <mgarciaberge@gmail.com>
2021-10-15 20:33:20 +0200

    primer commit

---
+++ print_date.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p /home/manel/test_script
+echo "hola son las $(date +%T) del $(date +%a%F)" >> /home/manel/test_script/salutacio.txt
+
---
+++ test2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define the filename
+filename='books.txt'
+
+# Type the text that you want to append
+read -p "Enter the text that you want to append:" newtext
+
+# Check the new text is empty or not
+if [ "$newtext" != "" ]; then
+      # Append the text by using '>>' symbol
+      printf "Appended text is: %s\n" "$newtext" >> $filename
+fi
